daf1ccccf5e26b31f076a3f141ec9e3d13cfda0e
keona-i <57222054+keona-i@users.noreply.github.com>
2019-12-19 19:45:40 +0100

    kein plan

---
+++ Projekt/interface_vue/src/components/spieler1.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    
+   
+  </div>
+</template>
+
+<script>
+
+import io from "socket.io-client";
+
+export default {
+  name: 'spieler1',
+  data:function(){
+    return {
+      socket: io.connect("http://localhost:3000"),
+      result:"",
+    };
+  },
+  props: {
+    msg: String
+  },
+  methods: {
+    sendValues(){
+      this.socket.emit("addMessage", function(payload))
+      }
+    },
+    mounted(){
+      this.socket.on("test",payload =>{
+        this.result = payload;
+      });
+    }
+  };
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ Projekt/serve.js
@@ -0,0 +1,33 @@
+// Das Modul express wird aufgereufen und in der varibale express dargestellt
+const express = require('express');
+//der befehl um express zu starten und vor zubreiten
+const app = express();
+// die konstante Port wert, so kann man es ändern und es ändert alles im text 
+const port = 3000
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io');
+//verbinden das mit express da express http schon hat
+const io_server = io(http);
+
+
+
+
+
+
+io_server.on('connection', function(socket) {  //bestätigt die verbindung
+	 console.log("Connected")
+	 socket.on('addMessage', function(payload) {  //Sendet es nach socket 
+	 	socket.emit('test',test) // hört (Listen for event), in socket was abgefragt wird
+	 	console.log(test);
+	 });
+	});
+
+
+// listen = ein funktion von der express app .. hört welche anschluss ist also 3000
+// wenn es funktionoert dan macht es die zweite event 
+  
+// wenn nur () steht = function mit geschweifte Klammer 
+http.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`)}); 
