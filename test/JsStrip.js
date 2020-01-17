086d7a1123a639575b447f134647da4b46e15dbf
keona-i <57222054+keona-i@users.noreply.github.com>
2020-01-17 09:26:08 +0100

    arduino mit js ansteuren

--- test/JsStrip.js
+++ test/JsStrip.js
@@ -1,29 +1,40 @@
 const pixel = require("node-pixel");
 const firmata = require('firmata');
 
-let board = new firmata.Board('/dev/cu.usbmodem143201',function(){
+let board = new firmata.Board('/dev/cu.usbmodem143101',function(){
 
     strip = new pixel.Strip({
         strips: [ {pin: 6, length: 30}, ],
+        gamma: 1.2,
         firmata: board,
         controller: "FIRMATA",
     });
 
 strip.on("ready", function() {
 	// Set the entire strip to pink.
-	strip.color('#903');
+	strip.color('#0B1B1E');
 
 	// Set first and seventh pixels to turquoise.
-	strip.pixel(0).color('#074');
-	strip.pixel(6).color('#074');
+	strip.pixel(0).color('#1D3513');
+	strip.pixel(6).color('#1D3513');
+
 	// Display initial state.
 	strip.show();
 	// Loop the following command forever
 	// at 12fps until Arduino powers down.
-	let loop = setInterval(function () {
-	// Shift all pixels clockwise
-		strip.shift(1, pixel.FORWARD, true);
+
+	loop = setInterval(function () {
+	//Shift all pixels clockwise
+		 strip.shift(1, pixel.FORWARD, true);
 		strip.show();
 	    }, 1000 / 12);
 	});
+
+
+	
+	
+
+
+
+	
 });
