c0ffdbef420eee1ce45f0f51d93d38ed7c00d52b
keona-i <57222054+keona-i@users.noreply.github.com>
2020-02-18 14:24:04 +0100

    Update HelloWorld.vue

--- test/viergewinnt_ui/viergewinnt/src/components/HelloWorld.vue
+++ test/viergewinnt_ui/viergewinnt/src/components/HelloWorld.vue
@@ -10,6 +10,7 @@
       </b-row>
         <b-card no-body>
           <b-tabs card>
+	  // Die Tap für Spieler 1&2 und die Buttons mit Bootstrap vue.
             <b-tab title="Player 1" active>
               <b-card>
               	<b-button class="playbtn" pill variant="outline-secondary" v-on:click="sendValues(1,1)">1<br/>⬇</b-button>
@@ -47,11 +48,13 @@
 </template>
 
 <script>
+// holt sich die daten von socket.io-client.
   import io from "socket.io-client";
 
 export default {
   name: "HelloWorld",
   
+  // kann auf lokalhost:3000 angesehen werden
   data: function(){
     return {
       socket: io.connect("http://10.0.0.1:3000"),
@@ -64,7 +67,9 @@ export default {
     msg: String
   },
   //sendet es und ist die verbing zur Js-datei
-  methods:{
+  // wird auf den Button Play 1 auf 1 geklickt, schickt es die Daten welche Spalte es reingeworden wird und von welcher Spieler.
+ // es werden die daten für Reset an die Js-datei geschickt, die beiden Daten können im Terminal nach gelesen werden.
+ methods:{
     sendValues(spalt, player){
       this.socket.emit("playMessage",spalt, player); 
     },
@@ -83,6 +88,7 @@ export default {
     
 
 </script>
+// die CSS sind für die "Schönheit" und Gestaltung 
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
